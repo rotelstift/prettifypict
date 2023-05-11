b5a4856d32e21540d51d38145a17f09e3da224ea
rotelstift <rotelstift@gmail.com>
2023-05-11 09:08:48 +0900

    init frame

--- prettifypict/src/app/app.module.ts
+++ prettifypict/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PictureComponent } from './picture/picture.component';
+import { FrameComponent } from './frame/frame.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PictureComponent
+    PictureComponent,
+    FrameComponent
   ],
   imports: [
     BrowserModule,
